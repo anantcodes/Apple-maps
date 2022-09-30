36ad86042e9306e8992218c30d6e2dcafbb0647a
Anant Kanchan <anant4265@gmail.com>
2022-09-30 20:10:16 +0530

    initial commit

---
+++ AppleMaps/AppleMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMapsApp.swift
+//  AppleMaps
+//
+//  Created by Anant Kanchan on 30/09/22.
+//
+
+import SwiftUI
+
+@main
+struct AppleMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMaps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleMaps
+//
+//  Created by Anant Kanchan on 30/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
